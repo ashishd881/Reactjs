fdfb6b6640604b0a9c33e98c5c08dd0d2c1c4ecc
ashishd881 <ashishkpdh881@gmail.com>
2025-06-14 15:48:58 +0530

    Fix import path for authService in LogoutBtn component; add missing onClick handler to Logout button

--- 12MegaBlog/src/components/Header/LogoutBtn.jsx
+++ 12MegaBlog/src/components/Header/LogoutBtn.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import authService from '../../appwrite/config.js'
+import authService from '../../appwrite/auth.js'
 import {logout} from '../../store/authSlice.js'
 
 
@@ -16,7 +16,7 @@ function LogoutBtn() {
         }) 
     }
   return (
-    <button className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>Logout</button>
+    <button onClick={logoutHandler} className=' inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>Logout</button>
   )
 }
 
